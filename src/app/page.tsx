f988a52cf09fb222dcf038a5a2921bf647c16116
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2025-08-27 17:38:24 -0300

    refactor: streamline Home component layout and enhance image responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,9 @@ import { desc } from "drizzle-orm";
 import Image from "next/image";
 
 import Footer from "@/components/common/footer";
-import { Header } from "@/components/common/header";
 import ProductList from "@/components/common/product-list";
 import { db } from "@/db";
-import { categoryTable, productTable } from "@/db/schema";
+import { productTable } from "@/db/schema";
 
 async function Home() {
   const products = await db.query.productTable.findMany({
@@ -17,40 +16,74 @@ async function Home() {
     with: { variants: true },
   });
 
-  const categories = await db.query.categoryTable.findMany({});
-
   return (
     <>
-      <div className="space-y-6">
-        <div className="px-5">
-          <Image
-            src="/banner-01.png"
-            alt="Leve uma vida com estilo"
-            height={0}
-            width={0}
-            sizes="100vw"
-            className="h-auto w-full"
-          />
+      <div className="mx-auto max-w-screen-xl space-y-6 px-5 lg:space-y-12 lg:px-10">
+        <div>
+          <picture>
+            <source srcSet="/hero-desktop.png" media="(min-width: 1024px)" />
+
+            <Image
+              src="/banner-01.png"
+              alt="Leve uma vida com estilo"
+              height={0}
+              width={0}
+              sizes="100vw"
+              className="mt-6 h-auto w-full rounded-l-2xl"
+            />
+          </picture>
         </div>
 
         <ProductList products={products} title="Mais vendidos" />
 
-        <div className="px-5">
-          <Image
-            src="/banner-02.png"
-            alt="Leve uma vida com estilo"
-            height={0}
-            width={0}
-            sizes="100vw"
-            className="h-auto w-full"
-          />
+        <div>
+          <div className="block md:hidden">
+            <Image
+              src="/banner-02.png"
+              alt="Leve uma vida com estilo"
+              height={0}
+              width={0}
+              sizes="100vw"
+              className="h-auto w-full"
+            />
+          </div>
+
+          <div className="hidden md:grid md:grid-cols-2 md:gap-4">
+            <Image
+              src="/banner-02.png"
+              alt="Leve uma vida com estilo"
+              height={0}
+              width={0}
+              sizes="50vw"
+              className="h-auto w-full rounded-2xl"
+            />
+
+            <div className="grid grid-rows-2 gap-4">
+              <Image
+                src="/Frame222.png"
+                alt="Produto destaque"
+                height={0}
+                width={0}
+                sizes="50vw"
+                className="h-auto w-full rounded-2xl"
+              />
+              <Image
+                src="/Frame224.png"
+                alt="Produto destaque"
+                height={0}
+                width={0}
+                sizes="50vw"
+                className="h-auto w-full rounded-2xl"
+              />
+            </div>
+          </div>
         </div>
 
         <ProductList products={newlyCreatedProducts} title="Novos produtos" />
+      </div>
 
-        <div className="mt-12">
-          <Footer />
-        </div>
+      <div className="mt-12">
+        <Footer />
       </div>
     </>
   );
